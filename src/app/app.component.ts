44dcaa77403b8d5ac5f617f9713414ddb59e959e
Bjoern <b.r.deleuran@hotmail.com>
2020-08-12 10:35:26 +0200

    do it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'diktatorerne';
+  registerForm: any;
+  deleteForm: any;
+  entry: Array<any>;
+  counter: number;
+  data1: [object] = [Object];
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      birthYear: '',
+      deathYear: ''
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  register(user: object) {
+    console.log(user);
+    this.counter += 1;
+    this.data1.push(user);
+    console.log(this.data1);
+  }
+
+  deleteLatest() {
+    this.data1.splice(-1, 1);
+  }
 }
